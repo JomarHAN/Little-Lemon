c792e8047b9263f372e1f5270cf9a759decf1f53
Jomar <jomarhan.ng@gmail.com>
2023-01-19 10:45:25 -0500

    styling responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,30 +5,33 @@ function Footer() {
   return (
     <footer>
       <div className='container-row footer-content'>
-        <img src={logo} alt="" />
-        <div className='footer-navigation'>
-          <h4>Navigation</h4> 
-          <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/menu">Menu</a></li>
-              <li><a href="/reservation">Reservation</a></li>
-              <li><a href="/order-online">Order Online</a></li>
-              <li><a href="/login">Login</a></li>
-          </ul>
+        <img className='footer-logo' src={logo} alt="" />
+        <div className="footer-details">
+          <div className='footer-navigation'>
+            <h4>Navigation</h4> 
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reservation">Reservation</a></li>
+                <li><a href="/order-online">Order Online</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+          </div>
+        <div className='footer-contact'>
+              <h4>Contact</h4>
+              <p>Address</p>
+              <p>Phone Number</p>
+              <p>Email</p>
         </div>
-       <div className='footer-contact'>
-            <h4>Contact</h4>
-            <p>Address</p>
-            <p>Phone Number</p>
-            <p>Email</p>
-       </div>
-       <div className='footer-social'>
-            <h4>Social Media</h4>
-            <p>Address</p>
-            <p>Phone Number</p>
-            <p>Email</p>
-       </div>
+        <div className='footer-social'>
+              <h4>Social Media</h4>
+              <p>Address</p>
+              <p>Phone Number</p>
+              <p>Email</p>
+        </div>
+      </div>
+
       </div>
     </footer>
   )
